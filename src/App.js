4115d2cbac52d4fd0cc594d19a54dc168be63cf7
Mauricio-OC <murisc0@hotmail.com>
2022-08-25 19:36:56 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,14 +1,118 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Form from './components/Form';
+import Card from './components/Card';
 
-class App extends React.Component {
+export default class App extends Component {
+  state = {
+    cardName: '',
+    cardDescription: '',
+    cardAttr1: '0',
+    cardAttr2: '0',
+    cardAttr3: '0',
+    cardImage: '',
+    cardRare: 'normal',
+    cardTrunfo: false,
+    hasTrunfo: false,
+    isSaveButtonDisabled: true,
+    cards: [],
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+      } = this.state;
+      const min = 0;
+      const max = 90;
+      const total = 210;
+      const cardSum = +cardAttr1 + +cardAttr2 + +cardAttr3;
+      if (cardName
+        && cardDescription
+        && cardImage
+        && cardAttr1 <= max
+        && cardAttr2 <= max
+        && cardAttr3 <= max
+        && cardAttr1 >= min
+        && cardAttr2 >= min
+        && cardAttr3 >= min
+        && (cardSum <= total)
+      ) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
+    });
+  };
+
+  onSaveButtonClick = (card) => {
+    const { cards } = this.state;
+    this.setState(() => ({
+      cards: [...cards, card],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+    }));
+  };
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+    } = this.state;
+
     return (
       <div>
         <h1>Tryunfo</h1>
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardImage={ cardImage }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
 }
-
-export default App;
-// 
+// INCIANDO! vrummm
